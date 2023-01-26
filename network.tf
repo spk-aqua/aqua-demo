156271976259fbd8db8c9b04f3b04cba26de4039
SPK-Jungho <121412787+SPK-Jungho@users.noreply.github.com>
2023-01-26 22:16:40 +0900

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,13 +2,6 @@ resource "aws_security_group" "main" {
   name        = "main"
   description = "Default SG for instances"
   vpc_id      = local.vpc_id
-
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-  }
   
   
   ingress {
